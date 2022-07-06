3e8c5093b29c6937a78551862dc6633407deb5d9
Belcross17 <alfredochabes1@hotmail.com>
2022-07-06 14:53:57 -0400

    variables set in JS using their corresponding IDs

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,8 +1,18 @@
-// global variable
+// global variables
 var viewHighScoresEl = document.getElementById("view-high-score");
 var timeLeftEl = document.getElementById("time-left");
 var contentEl = document.getElementById("content");
-var displayQuestionEl = document.getElementById("display-question")
+var displayQuestionEl = document.getElementById("display-question");
+var displayFinalScoreEl = document.getElementById("display-final-score");
+var enterInitialsEL = document.getElementById("enter-initials");
+var enterInitialsTextAreaEl = document.getElementById("enter-initials-text-area");
+var startQuizEl =  document.getElementById("start-quiz");
+var answer1BtnEl = document.getElementById("answer1");
+var answer2BtnEl = document.getElementById("answer2");
+var answer3BtnEl = document.getElementById("answer3");
+var answer4BtnEl = document.getElementById("answer4");
+var submitScore = document.getElementById("submit-score");
+var answerValidationEl = document.getElementById("answer-validation");
 
 // hide answer section while empty
 
