9fba67a7fa4fe6dc95626a6c7ae1b81bbfa348dc
Belcross17 <alfredochabes1@hotmail.com>
2022-07-06 12:36:50 -0400

    plan to struction JS, using comments

---
+++ assets/js/java.js
@@ -0,0 +1,16 @@
+// global variable
+
+
+// hide answer section while empty
+
+//questions
+
+//all available answers
+
+//start timer
+
+//function to check each question
+
+//function to check answer
+
+//event listeners
