d6c2de46830640805d6bed0ba6b8a03006b3f87d
Belcross17 <alfredochabes1@hotmail.com>
2022-07-06 19:55:37 -0400

    divided functions thru 3 different JS files

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,3 @@
-//Collaborated with Janvier Mbilizi
-
-// list of all questions, choices, and answers
 var questions = [
     {
       title: "Which one is a looping structure in JavaScript?",
@@ -42,47 +39,5 @@ var questions = [
         "Logical Errors"
       ],
       answer: "Animation time errors"
-    },
-    {
-      title: "What is the data type of variables in JavaScript?",
-      choices: [
-        "Object data types",
-        "Function data type",
-        "None of the above",
-        "All of the above"
-      ],
-      answer: "Object data types"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
-    },
-    {
-      title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    },
-    {
-      title: "What is the type of Pop up boxes available in JavaScript?:",
-      choices: ["Alert", "Confirm", "Prompt", "All the above"],
-      answer: "All the above"
     }
   ];
--- assets/js/score.js
+++ assets/js/score.js
@@ -1,5 +1,3 @@
-//Collaborated with Janvier Mbilizi
-
 function printHighscores() {
     // either get scores from localstorage or set to empty array
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
